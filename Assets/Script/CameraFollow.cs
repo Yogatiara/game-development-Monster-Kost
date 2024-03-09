a7dce824d1a0530d40a489acee151ca4969f0626
unknown <11211086@student.itk.ac.id>
2024-03-09 21:15:43 +0800

    camera following player

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        if (player != null)
+        {
+            Vector3 targetPosition = player.position + offset;
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+        }
+        // transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, -10f);
+
+
+    }
+}
